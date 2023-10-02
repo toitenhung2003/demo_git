64955c8edba0043011398c8e5c3c4a46d8e95235
TruongNguyen <truongnnph24742@fpt.edu.vn>
2023-10-02 20:34:47 +0700

    truong_test

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tôi tên hưng</Text>
+      <Text> Trường </Text>
       <StatusBar style="auto" />
     </View>
   );
